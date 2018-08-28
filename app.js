033129766eb0bcf633d78d81e7db11b3393b5e5f
Juan Zapien <jmzapien@ucsd.edu>
2018-08-28 13:03:46 -0700

    chnaging port

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.get('/about', function (req, res) {
     res.render('about');
 });
 
+app.set("port", process.env.PORT || 3000);
 
 
 
@@ -26,7 +27,6 @@ app.get('/about', function (req, res) {
 });
 
 */
-
-app.listen(3000, function(){
-	console.log('Server started at port 3000');
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Express server listening on port " + app.get("port"));
 });
