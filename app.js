bd7af711f50dec65c0939bc1ba0bc9d243ac19c1
Juan Zapien <jmzapien@ucsd.edu>
2018-09-02 00:43:31 -0700

    heroku

--- app.js
+++ app.js
@@ -4,18 +4,34 @@ var http = require('http');
 var path = require("path");
 var exphbs  = require('express-handlebars');
 
+
+
+
+var home = require("./routes/home");
+var about = require("./routes/about");
+
+
 var app = express();
 
 
+//environments
 app.set("port", process.env.PORT || 3000);
+
+
+
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, "public")));
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 
 var home = require("./routes/home");
 var about = require("./routes/about");
+
+/*
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}*/
 
 
 
@@ -25,12 +41,12 @@ app.get('/about', about.view);
 app.set("port", process.env.PORT || 3000);
 
 
-
-/*app.get('/', function(req, res){
-	res.send('Hello World');
-});
-
-*/
+/*
 app.listen(process.env.PORT || 3000, function() {
   console.log("Express server listening on port " + app.get("port"));
+});*/
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
