d55c3aca8be58d096dd3f6d8bfa30586f6771e2b
Juan Zapien <jmzapien@ucsd.edu>
2018-09-01 23:08:59 -0700

    deleting extra files

--- CSE3/CSE3/Hw5/HelloWorld.py
+++
@@ -1,8 +0,0 @@
-#<Juan Zapien> <wednesday at 4>
-#<2/17/2016> Hw5
-print("Hello World!")
-my_name=raw_input("What is your name?")
-print("Hello"+ my_name)
-my_major= raw_input("What is your major?")
-my_year= raw_input("What year are you?")
-print("You are a " + my_year  +  " year "  + my_major + " major.")
