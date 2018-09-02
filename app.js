3df3aae5cf0ad7bd33b6003bd371f89832572821
Juan Zapien <jmzapien@ucsd.edu>
2018-09-01 22:45:15 -0700

    fixing heroku bug

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const express = require('express');
-const exphbs  = require('express-handlebars');
-const path = require("path");
 
-const app = express();
+var express = require('express');
+var http = require('http');
+var path = require("path");
+var exphbs  = require('express-handlebars');
+
+var app = express();
 
 
+app.set("port", process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, "public")));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
