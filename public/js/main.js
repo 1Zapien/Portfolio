c0b578c273a8fd16f7882f046bcb28a4d1d35b3d
1zapien <guan235@gmail.com>
2022-04-03 02:45:56 -0700

    testing new images

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,29 @@
-$(window).on('load', function() {
-  $('.post-module').hover(function() {
-    $(this).find('.description').stop().animate({
-      height: "toggle",
-      opacity: "toggle"
-    }, 300);
+$(window).on("load", function() {
+  $(".post-module").hover(function() {
+    $(this)
+      .find(".description")
+      .stop()
+      .animate(
+        {
+          height: "toggle",
+          opacity: "toggle"
+        },
+        300
+      );
   });
 });
 
-
 var path = location.pathname; // ex: '/profile';
 
-
-$(function(){
+$(function() {
   var current = location.pathname;
-  $('.navvar a').each(function(){
-      var $this = $(this);
-      // if the current path is like this link, make it active
-      if($this.attr('id') == current){
-          $this.removeClass();
-          $this.addClass('active');
-          return;
-      }
-  })
-})
-
+  $(".navvar a").each(function() {
+    var $this = $(this);
+    // if the current path is like this link, make it active
+    if ($this.attr("id") == current) {
+      // $this.removeClass();
+      $this.addClass("active");
+      return;
+    }
+  });
+});
